d543a127319ea6f332876fd663a53c133e12a312
Geovanny Juárez <admin@Geovannys-MacBook-Pro.local>
2026-07-28 17:18:37 -0600

    feat: use user-add icon for send employees button

--- resources/views/livewire/devices/device-manager.blade.php
+++ resources/views/livewire/devices/device-manager.blade.php
@@ -672,7 +672,7 @@ new class extends Component {
                             <button wire:click="openImportModal({{ $device->id }})" title="Enviar empleados al dispositivo"
                                 class="text-yellow-500 hover:text-yellow-600">
                                 <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"/>
                                 </svg>
                             </button>
                             {{-- Editar --}}
